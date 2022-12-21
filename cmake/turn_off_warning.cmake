443e0668228346794000439ff8ac16a712046882
life4gal <life4gal@gmail.com>
2022-12-21 18:49:29 +0800

    Try to fix the compilation problem of ClangCl.

--- cmake/turn_off_warning.cmake
+++ cmake/turn_off_warning.cmake
@@ -1,35 +1,47 @@
 set(
-	GSL_MSVC_WARNINGS 
+		GSL_MSVC_WARNINGS
 
-	/Wv:18
+		/Wv:18
 )
 
 set(
-	GSL_GNU_WARNINGS
-
-)
-
-set(
-	GSL_CLANG_CL_WARNINGS
+		GSL_GNU_WARNINGS
 
 )
 
 set(
-	GSL_CLANG_WARNINGS
+		GSL_CLANG_CL_WARNINGS
 
+		# for ut :(
+		-Wno-global-constructors
+		-Wno-exit-time-destructors
+		-Wno-ctad-maybe-unsupported
+		-Wno-missing-variable-declarations
+		-Wno-sign-compare
+)
+
+set(
+		GSL_CLANG_WARNINGS
+
+		# for ut :(
+		-Wno-global-constructors
+		-Wno-exit-time-destructors
+		-Wno-ctad-maybe-unsupported
+		-Wno-missing-variable-declarations
+		-Wno-sign-compare
 )
 
 macro(turn_off_warning target_name)
 	message("Turn off compile warning for project ${target_name}...")
 	target_compile_options(
-		${target_name}
-		PRIVATE
+			${target_name}
+			PRIVATE
 
-		$<$<CXX_COMPILER_ID:MSVC>:${GSL_MSVC_WARNINGS}>
-		$<$<CXX_COMPILER_ID:GNU>:${GSL_GNU_WARNINGS}>
-		# clang-cl
-		$<$<AND:$<CXX_COMPILER_ID:Clang>,$<STREQUAL:"${CMAKE_CXX_SIMULATE_ID}","MSVC">>:${GSL_CLANG_CL_WARNINGS}>
-		# clang
-		$<$<AND:$<CXX_COMPILER_ID:Clang>,$<NOT:$<STREQUAL:"${CMAKE_CXX_SIMULATE_ID}","MSVC">>>:${GSL_CLANG_WARNINGS}>
+			$<$<CXX_COMPILER_ID:MSVC>:${GSL_MSVC_WARNINGS}>
+			$<$<CXX_COMPILER_ID:GNU>:${GSL_GNU_WARNINGS}>
+			# clang-cl
+			$<$<AND:$<CXX_COMPILER_ID:Clang>,$<STREQUAL:"${CMAKE_CXX_SIMULATE_ID}","MSVC">>:${GSL_CLANG_CL_WARNINGS}>
+			# clang
+			$<$<AND:$<CXX_COMPILER_ID:Clang>,$<NOT:$<STREQUAL:"${CMAKE_CXX_SIMULATE_ID}","MSVC">>>:${GSL_CLANG_WARNINGS}>
 	)
 endmacro()
