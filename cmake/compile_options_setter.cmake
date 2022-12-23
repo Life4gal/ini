2953afc538fba51a3c58f4056515a61ee961bf49
life4gal <life4gal@gmail.com>
2022-12-23 09:42:30 +0800

    Add AppleClang check.

--- cmake/compile_options_setter.cmake
+++ cmake/compile_options_setter.cmake
@@ -12,13 +12,19 @@ function(set_compile_property target_name)
 				PROPERTIES
 				COMPILE_OPTIONS "/W4;/WX"
 		)
-	elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+	elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
 		set_target_properties(
 				${target_name}
 				PROPERTIES
 				COMPILE_OPTIONS "-Wall;-Wextra;-Wpedantic;-Werror"
 		)
-	else()
+	elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang")
+		set_target_properties(
+				${target_name}
+				PROPERTIES
+				COMPILE_OPTIONS "-Wall;-Wextra;-Wpedantic;-Werror"
+		)
+	else ()
 		message("Unknown Compiler ID: ${CMAKE_CXX_COMPILER_ID}")
-	endif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+	endif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
 endfunction(set_compile_property)
